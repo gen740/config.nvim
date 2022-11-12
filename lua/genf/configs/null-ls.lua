41c13d8a29e1dec60c22fb49360aac2716497884
gen740 <genfujimoto@gmail.com>
2022-11-12 11:13:03 +0900

    Update

---
+++ lua/genf/configs/null-ls.lua
@@ -0,0 +1,21 @@
+local function null_ls_setup()
+  local null_ls = require('null-ls')
+  null_ls.setup {
+    sources = {
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.perltidy,
+      null_ls.builtins.formatting.shfmt,
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.isort,
+      null_ls.builtins.formatting.markdownlint,
+      null_ls.builtins.diagnostics.markdownlint,
+      -- null_ls.builtins.diagnostics.cspell,
+      -- null_ls.builtins.code_actions.cspell,
+      null_ls.builtins.formatting.latexindent,
+    },
+  }
+end
+
+return {
+  setup = null_ls_setup,
+}
--- lua/genf/plugins.lua
+++ lua/genf/plugins.lua
@@ -1,5 +1,6 @@
--- Auto Packer Install Start
+---@diagnostic disable-next-line
 local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+
 if not packer_exists then
   if vim.fn.input('Download Packer? (y for yes)') ~= 'y' then
     return
@@ -145,26 +146,7 @@ require('packer').startup {
       },
       config = config('lsp'),
     }
-    use {
-      'jose-elias-alvarez/null-ls.nvim',
-      config = function()
-        local null_ls = require('null-ls')
-        null_ls.setup {
-          sources = {
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.formatting.perltidy,
-            null_ls.builtins.formatting.shfmt,
-            null_ls.builtins.formatting.black,
-            null_ls.builtins.formatting.isort,
-            null_ls.builtins.formatting.markdownlint,
-            null_ls.builtins.diagnostics.markdownlint,
-            -- null_ls.builtins.diagnostics.cspell,
-            -- null_ls.builtins.code_actions.cspell,
-            null_ls.builtins.formatting.latexindent,
-          },
-        }
-      end,
-    }
+    use { 'jose-elias-alvarez/null-ls.nvim', config = config('null-ls') }
     use {
       'rcarriga/nvim-dap-ui',
       requires = {
