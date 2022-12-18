3c2a3e95d1e136f4858757c9c621a6bd0a0998f2
gen740 <genfujimoto@gmail.com>
2022-12-18 19:49:39 +0900

    Suppress lua startup warning.

--- lua/genf/language_services/lua.lua
+++ lua/genf/language_services/lua.lua
@@ -23,6 +23,7 @@ function M.lsp_config()
         },
         workspace = {
           library = vim.api.nvim_get_runtime_file('', true),
+          checkThirdParty = false,
         },
         telemetry = {
           enable = false,
