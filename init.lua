8f9d49e8b40d319692dcbd3b34d2347fe6036142
gen740 <genfujimoto@gmail.com>
2023-01-31 02:32:19 +0900

    Update

--- init.lua
+++ init.lua
@@ -6,9 +6,22 @@ elseif dir_exists('~/.config/nvim-venv/bin') then
   vim.g.python3_host_prog = '~/.config/nvim-venv/bin/python3'
 end
 
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
 require('genf.autocmd')
 require('genf.options')
-require('genf.plugins')
+require('lazy').setup('plugins')
 require('genf.keymap')
 require('genf.commands')
 require('genf.statusline')
