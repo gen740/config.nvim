8bea6235ecdc5aaf9d1b3a501078dc3ac10042a4
gen740 <gen740.f@gmail.com>
2023-11-01 00:13:13 +0900

    Add fern reload keymap

--- lua/genf/language_services/fern.lua
+++
@@ -1,8 +0,0 @@
-M = {}
-
-function M.setup()
-  vim.opt_local.number = false
-  vim.opt_local.relativenumber = false
-end
-
-return M
--- lua/plugins/fern.lua
+++ lua/plugins/fern.lua
@@ -7,12 +7,22 @@ return {
     'lambdalisue/fern-hijack.vim',
   },
   cmd = 'Fern',
-  init = function()
-    vim.g['loaded_fern_git_status'] = 1
-  end,
   config = function()
     vim.fn['fern_git_status#init']()
     vim.g['fern#renderer'] = 'nerdfont'
     vim.g['fern#hide_cursor'] = 1
+    vim.api.nvim_create_autocmd({ 'Filetype' }, {
+      pattern = 'fern',
+      callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+        vim.keymap.set(
+          'n',
+          'r',
+          '<Plug>(fern-action-reload:all)<cr>',
+          { buffer = true, noremap = true, silent = true }
+        )
+      end,
+    })
   end,
 }
