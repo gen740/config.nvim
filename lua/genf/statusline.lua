8851f09344efc3829229f29bdeeb9d74c6e5d47f
gen740 <gen740.f@gmail.com>
2023-11-08 00:34:35 +0900

    Add Custom tabbar

--- lua/genf/statusline.lua
+++ lua/genf/statusline.lua
@@ -1,5 +1,25 @@
 vim.opt.statusline = [[%{''}]]
 
-function MyTabLine() end
+function MyTabLine()
+  local s = ''
+  for i = 1, vim.fn.tabpagenr('$') do
+    if i == vim.fn.tabpagenr() then
+      s = s .. '%#TabLineSel#'
+    else
+      s = s .. '%#TabLine#'
+    end
 
-vim.opt.tabline = [[v:lua.MyTabLine()]]
+    local bufname =
+      vim.fn.bufname(vim.fn.tabpagebuflist(i)[vim.fn.tabpagewinnr(i)])
+    if bufname == '' then
+      s = s .. ' ' .. '[No Name]' .. ' '
+    else
+      s = s .. ' ' .. bufname .. ' '
+    end
+  end
+
+  s = s .. '%#TabLineFill#%T'
+  return s
+end
+
+vim.opt.tabline = '%!v:lua.MyTabLine()'
