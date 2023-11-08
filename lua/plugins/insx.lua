c92f4708c55a84d80f9dae19b27e13a7479029a2
gen740 <gen740.f@gmail.com>
2023-11-08 16:49:57 +0900

    Let automatically load lsp setting.

--- lua/plugins/insx.lua
+++ lua/plugins/insx.lua
@@ -2,6 +2,5 @@ return {
   'hrsh7th/nvim-insx',
   config = function()
     require('insx.preset.standard').setup()
-    local insx = require('insx')
   end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,26 +6,18 @@ return {
         require('genf.language_services.' .. name).lsp_config()
       end
 
-      local languages = {
-        'cmake', --
-        'cpp',
-        'dockerfile',
-        'go',
-        'javascript',
-        'typescript',
-        'typescriptreact',
-        'json',
-        'lua',
-        'python',
-        'rust',
-        'swift',
-        'tex',
-        'toml',
-        'yaml',
-      }
-
-      for _, lang in ipairs(languages) do
-        load_config(lang)
+      local config_dir = vim.fn.stdpath('config')
+        .. '/lua/genf/language_services'
+      local handle = vim.loop.fs_scandir(config_dir)
+      while handle do
+        local name, _ = vim.loop.fs_scandir_next(handle)
+        if not name then
+          break
+        end
+        local language_file = name:match('([^/\\]+)%.lua$')
+        if language_file then
+          pcall(load_config, language_file)
+        end
       end
     end,
   },
