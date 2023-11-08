0e26c9890c7bfa7ddde5bbe69a7ee7d4ea1b94df
gen740 <gen740.f@gmail.com>
2023-11-08 11:22:31 +0900

    Delete unused treesitter plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   config = function()
-    vim.g.matchup_matchparen_offscreen = {}
     require('nvim-treesitter.configs').setup {
-      autotag = {
-        enable = true,
-      },
       ensure_installed = {
         'c',
         'cpp',
@@ -25,7 +21,7 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
-        disable = function(lang, bufnr)
+        disable = function(_, bufnr)
           local byte_size = vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr))
           if byte_size > 512 * 1000 then
             return true
@@ -37,21 +33,15 @@ return {
       matchup = {
         enable = true,
       },
-      context_commentstring = {
-        enable = true,
-      },
     }
   end,
   dependencies = {
-    { 'andymass/vim-matchup' },
-    { 'nvim-treesitter/playground', cmd = 'TSPlaygroundToggle' },
-    { 'JoosepAlviste/nvim-ts-context-commentstring' },
     {
-      'windwp/nvim-ts-autotag',
+      'andymass/vim-matchup',
       config = function()
-        require('nvim-ts-autotag').setup {}
+        vim.g.matchup_matchparen_offscreen = { method = 'popup' }
       end,
-      ft = { 'html', 'js', 'ts' },
     },
+    { 'nvim-treesitter/playground', cmd = 'TSPlaygroundToggle' },
   },
 }
