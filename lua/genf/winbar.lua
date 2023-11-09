b496d02c0d28877b07feacd36a7ee2c8665082f9
gen740 <gen740.f@gmail.com>
2023-11-09 15:10:21 +0900

    Fix winbar bug

--- lua/genf/winbar.lua
+++ lua/genf/winbar.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local file_icon = function()
-  return require('nvim-web-devicons').get_icon_by_filetype(vim.api.nvim_get_option_value('ft', { buf = 0 }))
+  return [[%{luaeval("require('nvim-web-devicons').get_icon_by_filetype(vim.api.nvim_buf_get_option(0, 'filetype'))")}]]
 end
 
 local in_search = function()
@@ -14,6 +14,7 @@ local search_count = function()
     stats == nil
     or stats.total == nil
     or stats.maxcount == nil
+    or stats.current == nil
     or ((stats.current == 0 or stats.exact_match ~= 1) and not in_search())
   then
     return ''
@@ -29,7 +30,7 @@ local search_count = function()
   elseif stats.incomplete == 2 then -- maxcount exceed
     if stats.total > stats.maxcount and stats.current > stats.maxcount then
       return string.format('%s%s [>%d/>%d]', search_char, vim.fn.getreg('/'), stats.current, stats.total)
-    elseif stats.total > stats.maxcout then
+    elseif stats.total > stats.maxcount then
       return string.format('%s%s [%d/>%d]', search_char, vim.fn.getreg('/'), stats.current, stats.total)
     end
   end
@@ -54,10 +55,6 @@ local lsp_status = function()
 end
 
 M.expr = function()
-  local filetype = vim.bo.filetype
-  if filetype == 'fern' then
-    return '   Fern '
-  end
   return string.format(
     ' %s %%#WinBarFileName#%%f%%* %%M %s%%=%s %s',
     file_icon(),
