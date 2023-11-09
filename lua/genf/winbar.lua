4d2d246f795f73cc5c6c883070e521f454c171f6
gen740 <gen740.f@gmail.com>
2023-11-09 13:15:26 +0900

    Better search count

--- lua/genf/winbar.lua
+++ lua/genf/winbar.lua
@@ -4,9 +4,18 @@ local file_icon = function()
   return require('nvim-web-devicons').get_icon_by_filetype(vim.api.nvim_get_option_value('ft', { buf = 0 }))
 end
 
+local in_search = function()
+  return vim.fn.getcmdtype() == '/' or vim.fn.getcmdtype() == '?'
+end
+
 local search_count = function()
   local stats = vim.fn.searchcount { maxcount = 999 }
-  if stats == nil or stats.total == nil or stats.maxcount == nil or stats.current == 0 or stats.exact_match ~= 1 then
+  if
+    stats == nil
+    or stats.total == nil
+    or stats.maxcount == nil
+    or ((stats.current == 0 or stats.exact_match ~= 1) and not in_search())
+  then
     return ''
   end
 
@@ -16,7 +25,7 @@ local search_count = function()
   end
 
   if stats.incomplete == 1 then
-    return
+    return ''
   elseif stats.incomplete == 2 then -- maxcount exceed
     if stats.total > stats.maxcount and stats.current > stats.maxcount then
       return string.format('%s%s [>%d/>%d]', search_char, vim.fn.getreg('/'), stats.current, stats.total)
@@ -24,7 +33,7 @@ local search_count = function()
       return string.format('%s%s [%d/>%d]', search_char, vim.fn.getreg('/'), stats.current, stats.total)
     end
   end
-  return string.format('%s%s [%d/%d]', stats.current, stats.total, search_char, vim.fn.getreg('/'))
+  return string.format('%s%s [%d/%d]', search_char, vim.fn.getreg('/'), stats.current, stats.total)
 end
 
 local gitbranch = require('genf.gitbranch')
@@ -48,7 +57,7 @@ M.expr = function()
   return string.format(
     '%s %%#WinBarFileName#%%f%%* %%M %s%%=%s %s',
     file_icon(),
-    search_count(),
+    vim.print(search_count()),
     lsp_status(),
     git_branch()
   )
