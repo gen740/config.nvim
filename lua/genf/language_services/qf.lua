19271dc81e65bee5b5df12cdb79bb473aca2a605
gen740 <gen740.f@gmail.com>
2023-11-19 15:31:32 +0900

    Show number in quickfix

--- lua/genf/language_services/qf.lua
+++ lua/genf/language_services/qf.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
   vim.opt_local.cursorline = false
-  vim.opt_local.number = false
+  vim.opt_local.number = true
   vim.opt_local.relativenumber = false
   vim.opt_local.colorcolumn = '0'
   vim.opt_local.buflisted = false
