1d1540d289bbc69f40075c199f1fc6a3560ef638
gen740 <gen740.f@gmail.com>
2023-11-27 18:32:26 +0900

    Add cmake snippet

--- lua/genf/language_services/cmake.lua
+++ lua/genf/language_services/cmake.lua
@@ -4,9 +4,9 @@ local lmap = require('genf.language_services.utils').set_local_map
 local async_format = require('genf.language_services.utils').async_format
 
 function M.setup()
-  vim.opt_local.tabstop = 4
-  vim.opt_local.softtabstop = 4
-  vim.opt_local.shiftwidth = 4
+  vim.opt_local.tabstop = 2
+  vim.opt_local.softtabstop = 2
+  vim.opt_local.shiftwidth = 2
 
   lmap('n', '<space>f', function()
     async_format('cmake-format -i ' .. vim.fn.expand('%:p'))
@@ -14,6 +14,10 @@ function M.setup()
 
   lmap('n', '<m-c>', function()
     require('genf.asyncrun').asyncrun('task cmake')
+  end)
+
+  lmap('n', '<m-s>', function()
+    require('genf.asyncrun').asyncstop()
   end)
 
   vim.cmd('compiler cmake')
