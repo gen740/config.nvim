bcf580934a96bc8121759af478c1e8e1e95775ba
gen740 <gen740.f@gmail.com>
2023-12-11 19:06:38 +0900

    Delete dap

--- lua/genf/language_services/cpp.lua
+++ lua/genf/language_services/cpp.lua
@@ -48,28 +48,4 @@ function M.lsp_config()
   end
 end
 
-function M.dap_config()
-  local dap = require('dap')
-
-  dap.adapters.lldb = {
-    type = 'executable',
-    command = '/usr/local/opt/llvm/bin/lldb-vscode', -- adjust as needed
-    name = 'lldb',
-  }
-  dap.configurations.cpp = {
-    {
-      name = 'Launch',
-      type = 'lldb',
-      request = 'launch',
-      program = function()
-        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      end,
-      cwd = [[${workspaceFolder}]],
-      stopOnEntry = false,
-      runInTerminal = false,
-    },
-  }
-  dap.configurations.c = dap.configurations.cpp
-end
-
 return M
--- lua/genf/language_services/python.lua
+++ lua/genf/language_services/python.lua
@@ -43,31 +43,4 @@ function M.lsp_config()
   }
 end
 
-function M.dap_config()
-  local dap = require('dap')
-  dap.adapters.python = {
-    type = 'executable',
-    command = vim.fn.system('which python3'),
-    args = { '-m', 'debugpy.adapter' },
-  }
-  dap.configurations.python = {
-    {
-      type = 'python',
-      request = 'launch',
-      name = 'Launch file',
-      program = '${file}', -- This configuration will launch the current file if used.
-      pythonPath = function()
-        local cwd = vim.fn.getcwd()
-        if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-          return cwd .. '/venv/bin/python'
-        elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-          return cwd .. '/.venv/bin/python'
-        else
-          return '/Users/gen/.pyenv/shims/python3'
-        end
-      end,
-    },
-  }
-end
-
 return M
