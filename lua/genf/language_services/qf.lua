c0ecd6edd09955d4d4bcea7dfea191c85524ba73
gen740 <gen740.f@gmail.com>
2024-02-01 15:02:53 +0900

    [Update] Add qflist delete keymap

--- lua/genf/language_services/qf.lua
+++ lua/genf/language_services/qf.lua
@@ -1,10 +1,26 @@
 local M = {}
 
+local lmap = require('genf.language_services.utils').set_local_map
+
 function M.setup()
   vim.opt_local.cursorline = false
-  vim.opt_local.number = true
+  vim.opt_local.number = false
   vim.opt_local.relativenumber = false
   vim.opt_local.buflisted = false
+
+  lmap('n', 'd', function()
+    local qflist = vim.fn.getqflist()
+    local qf_len = #qflist
+    local new_qflist = {}
+    local current_line = vim.fn.line('.')
+    for idx, item in ipairs(qflist) do
+      if idx ~= current_line then
+        table.insert(new_qflist, item)
+      end
+    end
+    vim.fn.setqflist(new_qflist, 'r')
+    vim.api.nvim_win_set_cursor(0, { math.max(math.min(current_line, qf_len - 1), 1), 0 })
+  end)
 end
 
 return M
--- lua/genf/qftextfunc.lua
+++ lua/genf/qftextfunc.lua
@@ -3,14 +3,6 @@ local M = {}
 local f = vim.fn
 
 M.expr = function(info)
-  -- vim.cmd.syntax([[match FooBar '']])
-  --
-  local qfwinid = f.getqflist({ winid = 0 }).winid
-  vim.api.nvim_win_call(qfwinid, function()
-    vim.cmd.syntax([[match DiagnosticSignError '']])
-    vim.cmd.syntax([[match DiagnosticSignWarn '']])
-    vim.cmd.syntax([[match DiagnosticSignInfo '']])
-  end)
   local res = {}
   if info.quickfix == 1 then
     local qflist = f.getqflist({ id = info.id, items = 1 }).items
