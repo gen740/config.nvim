5cf6cd7ca73548cfaa3ae436d7dce1e6ea5d9354
gen740 <gen740.f@gmail.com>
2024-02-07 14:59:45 +0900

    [Update] Do not be italic comment & keywords

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,5 +25,13 @@ return {
   },
   {
     'folke/tokyonight.nvim',
+    config = function()
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+        },
+      }
+    end,
   },
 }
