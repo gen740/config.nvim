9ea2f194afe2a042974b07917a420cce17a5472a
gen740 <54583542+gen740@users.noreply.github.com>
2024-12-03 15:26:20 +0900

    fix(ruff): use ruff instead of ruff_lsp

--- lua/genf/plugins/lsps/ruff.lua
+++ lua/genf/plugins/lsps/ruff.lua
@@ -1,5 +1,5 @@
-if vim.fn.executable('ruff-lsp') == 0 then
+if vim.fn.executable('ruff') == 0 then
   return
 end
 
-require('genf.language_services.utils').lsp_setup('ruff_lsp')
+require('genf.language_services.utils').lsp_setup('ruff')
