e7bcb32f244c95a31898c28d964b5a482fbf6818
gen740 <54583542+gen740@users.noreply.github.com>
2025-02-25 19:40:20 +0900

    feat(plugins): require every plugins

--- lua/gen740/plugins/init.lua
+++ lua/gen740/plugins/init.lua
@@ -1,16 +1,4 @@
-local pdir_name = 'gen740/plugins'
-local base_path = vim.fn.stdpath('config') .. '/lua/' .. pdir_name
-
-local handle = vim.loop.fs_scandir(base_path)
-
-while handle do
-  local name, t = vim.loop.fs_scandir_next(handle)
-  if not name then
-    break
-  end
-  if name:match('%.lua$') and name ~= 'init.lua' then
-    require(pdir_name .. '.' .. name:gsub('%.lua$', ''))
-  elseif t == 'directory' then
-    require(pdir_name .. '.' .. name)
-  end
-end
+require('gen740.plugins.blink-cmp')
+require('gen740.plugins.copilot')
+require('gen740.plugins.oil')
+require('gen740.plugins.treesitter')
