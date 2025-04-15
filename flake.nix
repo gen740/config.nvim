28287542fa79de6fa4fedd9a40cc1f2b4660cfe5
gen740 <54583542+gen740@users.noreply.github.com>
2025-04-15 12:32:29 +0900

    feat(flake): Make config.nvim as a flake package

--- flake.nix
+++ flake.nix
@@ -1,24 +1,40 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+  };
   outputs =
-    { nixpkgs, flake-utils, ... }:
-
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShellNoCC {
-          packages = [
-            pkgs.lua-language-server
-            pkgs.stylua
-            pkgs.tokei
-          ];
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          packages.nvim-conf = pkgs.stdenv.mkDerivation {
+            pname = "nvim-conf";
+            version = "1.0.0";
+            src = ./.;
+            installPhase = ''
+              mkdir -p $out
+              cp -r * $out/
+            '';
+          };
+          devShells.default = pkgs.mkShellNoCC {
+            packages = [
+              pkgs.lua-language-server
+              pkgs.stylua
+              pkgs.tokei
+            ];
+          };
         };
-      }
-    );
+    };
 }
